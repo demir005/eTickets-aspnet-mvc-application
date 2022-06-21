3df50cd12b06f86eac7404556a54ac2e1af5bb12
DemirMahmutovic <demirmah93@gmail.com>
2022-06-21 12:27:29 +0200

    Models are added

---
+++ eTickets/Models/Actor_Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace eTickets.Models
+{
+    public class Actor_Movie
+    {
+        public int MovieId { get; set; }
+        public Movies Movies { get; set; }
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
