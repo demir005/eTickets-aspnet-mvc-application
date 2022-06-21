54d83f4e5e58c5ded26a2c2fd54400e5ba399627
DemirMahmutovic <demirmah93@gmail.com>
2022-06-21 13:09:34 +0200

    Controllers are added

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -52,6 +52,10 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed database
+
+            AppDbInitializer.Seed(app);
         }
     }
 }
